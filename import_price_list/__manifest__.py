831599de120929e21f6acf366e897ae696b3a5e4
marvidiaz <marvidiaz@gmail.com>
2018-06-28 20:42:17 -0400

    Update __manifest__.py

--- import_price_list/__manifest__.py
+++ import_price_list/__manifest__.py
@@ -20,15 +20,14 @@
 #
 ##############################################################################
 
-{
-    'name': 'Importar Tarifas desde CSV',
+    'name': 'Import price list CSV',
     'version': '10.0.1.0.0',
     'author': 'Falcon Solutions SpA',
     'maintainer': 'Falcon Solutions',
     'website': 'http://www.falconsolutions.cl',
     'license': 'AGPL-3',
     'category': 'Sales',
-    'summary': 'Importar Tarifas desde CSV',
+    'summary': '',
     'depends': ['base',
                 'sale',
                 ],
